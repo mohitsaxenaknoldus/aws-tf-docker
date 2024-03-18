6e0a11c18e39dd2d3ad679d84163d9ef21af33b8
Mohit Saxena <MSaxena@globeandmail.com>
2024-03-19 01:51:17 +0530

    add CICD

--- main.tf
+++ main.tf
@@ -42,3 +42,46 @@ resource "aws_ecr_repository" "my_repo" {
   name = "my-repo"
 }
 
+resource "aws_codebuild_project" "docker_build_project" {
+  name       = "docker-build-project"
+  description = "CodeBuild project to build Dockerfile and push image to ECR"
+  service_role = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    type            = "NO_SOURCE"
+    buildspec       = "buildspec.yml"
+  }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
+  name       = "codebuild-policy-attachment"
+  roles      = [aws_iam_role.codebuild_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"  # This policy provides full access to CodeBuild, adjust permissions as needed
+}
