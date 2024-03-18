2a4255098f5cc20cd45046722b61e51be027ebdb
Mohit Saxena <MSaxena@globeandmail.com>
2024-03-19 03:00:03 +0530

    add new project

--- main.tf
+++ main.tf
@@ -2,25 +2,95 @@ resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster"
 }
 
+resource "aws_iam_role_policy_attachment" "aws-tf-docker-ecr-policy-attachment" {
+  role       = aws_iam_role.aws-tf-docker-ecs_task_exec_role.name
+  policy_arn = aws_iam_policy.aws-tf-docker-ecr-policy.arn
+}
+
+resource "aws_iam_policy" "aws-tf-docker-ecr-policy" {
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:BatchGetImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload"
+      ],
+      "Resource": "${aws_ecr_repository.my_repo.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "aws-tf-docker-build-logs" {
+  name = "aws-tf-docker-build-logs"
+}
+
+resource "aws_iam_role" "aws-tf-docker-ecs_task_exec_role" {
+  name                 = "aws-tf-docker-ecs_task_exec_role"
+  max_session_duration = 10800
+  assume_role_policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecs_task_definition" "my_task_definition" {
-  family                = "my-task"
-  container_definitions = <<DEFINITION
-[
-  {
-    "name": "my-container",
-    "image": "${aws_ecr_repository.my_repo.repository_url}:latest",
-    "memory": 512,
-    "cpu": 256,
-    "portMappings": [
-      {
-        "containerPort": 3000,
-        "hostPort": 3000,
-        "protocol": "tcp"
+  family             = "aws-tf-docker-task-def"
+  memory             = "512"
+  network_mode       = "awsvpc"
+  cpu                = "256"
+  execution_role_arn = aws_iam_role.aws-tf-docker-ecs_task_exec_role.arn
+  requires_compatibilities = [
+    "FARGATE",
+  ]
+  container_definitions = jsonencode([
+    {
+      cpu    = 10
+      memory = 512
+      secrets = [
+      ]
+      environment = [
+      ]
+      image = "${aws_ecr_repository.my_repo.repository_url}:latest"
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.aws-tf-docker-build-logs.name
+          awslogs-region        = "us-east-1"
+          awslogs-stream-prefix = "aws-tf-docker"
+        }
       }
-    ]
-  }
-]
-DEFINITION
+      mountPoints = []
+      name        = "aws-tf-docker-container"
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        },
+      ]
+      volumesFrom = []
+    },
+  ])
 }
 
 resource "aws_ecs_service" "my_service" {
@@ -30,9 +100,8 @@ resource "aws_ecs_service" "my_service" {
   desired_count   = 1
 
   network_configuration {
-    subnets          = ["subnet-6e132a43", "subnet-8bf51cc3"] # Update with your subnet IDs
-    security_groups  = ["sg-bd6ebac2"]                        # Update with your security group IDs
-    assign_public_ip = true
+    subnets         = ["subnet-6e132a43", "subnet-8bf51cc3"] # Update with your subnet IDs
+    security_groups = ["sg-bd6ebac2"]                        # Update with your security group IDs
   }
 
   depends_on = [aws_ecs_cluster.my_cluster]
@@ -43,8 +112,8 @@ resource "aws_ecr_repository" "my_repo" {
 }
 
 resource "aws_codebuild_project" "docker_build_project" {
-  name       = "docker-build-project"
-  description = "CodeBuild project to build Dockerfile and push image to ECR"
+  name         = "docker-build-project"
+  description  = "CodeBuild project to build Dockerfile and push image to ECR"
   service_role = aws_iam_role.codebuild_role.arn
   artifacts {
     type = "NO_ARTIFACTS"
@@ -56,8 +125,12 @@ resource "aws_codebuild_project" "docker_build_project" {
     image_pull_credentials_type = "CODEBUILD"
   }
   source {
-    type            = "NO_SOURCE"
-    buildspec       = "buildspec.yml"
+    type            = "GITHUB"
+    location        = "https://github.com/mohitsaxenaknoldus/aws-tf-docker"
+    git_clone_depth = 1
+    git_submodules_config {
+      fetch_submodules = false
+    }
   }
 }
 
@@ -83,5 +156,5 @@ EOF
 resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
   name       = "codebuild-policy-attachment"
   roles      = [aws_iam_role.codebuild_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"  # This policy provides full access to CodeBuild, adjust permissions as needed
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess" # This policy provides full access to CodeBuild, adjust permissions as needed
 }
